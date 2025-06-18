877c57331763104f96c8fa7b6e57fa6612d5ffce
nikkjonee <nikkjone05@gmail.com>
2025-06-18 15:37:46 +0530

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -1,1 +1,10 @@
+project = 'systemmechanicsigninaccount'
+author = 'Your Name'
+release = '1.0'
 
+extensions = []
+templates_path = ['_templates']
+exclude_patterns = []
+
+html_theme = 'alabaster'
+html_static_path = ['_static']
